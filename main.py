8c46e126cc7b7f5a3f3e47f126749c8960aef625
judeadams21 <815386@mystma.org>
2021-03-22 14:42:31 +0000

    Jack and Jill (2011) Mad Lib

--- main.py
+++ main.py
@@ -4,4 +4,8 @@ actorTwo = "Also Adam Sandler"
 holidayTwo = "Hanukkah"
 name = "Jack"
 nameTwo = "Jill"
-print (holiday + " is usually a happy time, but ad executive Jack (" + actor + ") dreads the holiday because his twin sister, Jill (" + actorTwo + "), , makes her annual visit. When Jack and his sister get off on the wrong foot, the only way to make it right is to invite her to stay through " + holidayTwo + ".")
+adjective = "happy"
+relation = "twin sister"
+verb = "dreads"
+bodyPart = "foot"
+print (holiday + " is usually a " + adjective + " time, but ad executive Jack (" + actor + ") " + verb + " the holiday because his " + relation + ", Jill (" + actorTwo + "), makes her annual visit. When Jack and his sister get off on the wrong " + bodyPart + ", the only way to make it right is to invite her to stay through " + holidayTwo + ".")
