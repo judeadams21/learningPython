00c560bf5d5b97db0b98ef27c39a679884a61571
Adams Jude <815386@stma.k12.mn.us>
2021-03-26 10:01:29 -0500

    Selective Service PSA

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def addTwoNumbers(x, y):
-    return x + y
+age = input("How old are you?\n")
 
-print (addTwoNumbers(5, 7))
+if int(age) >= 18:
+    print("Well, it could be Moses in disguise. I have to let you in.")
+else:
+    print("Who does this kid this he is? No, no.")
